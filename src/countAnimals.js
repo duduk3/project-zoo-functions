10a70ba6bbf991e258663f12aee8be7eaba78e05
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-01-26 20:21:33 -0300

    requisito 5 => cria funçao de verificaçao e retorno com um parametro apenas

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const { species } = data;
+
+function countAnimals(animal, ...sexo) {
+  const specieNames = species.reduce((acc, specie) => ({ ...acc, [specie.name]: specie.residents.length }), {});
+  if (animal === '' || animal === undefined) {
+    return specieNames;
+  }
+  const { specie, sex } = animal;
+  if (!sex && species.some(({ name }) => name === specie)) {
+    return { [specie]: specieNames[specie] };
+  }
+  const animalsSpecie = species.filter((e) => e.location === 'NE');
+  return animalsSpecie;
 }
+
+console.log(countAnimals({ specie: 'lions' }));
 
 module.exports = countAnimals;
