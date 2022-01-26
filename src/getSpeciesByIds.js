31a8f26a859b7f9697a4171698cb3fb607451c4f
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-01-25 21:06:48 -0300

    requisito 1 => insere funçao com hof filter e for

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+const emptyArray = [];
+
+// eslint-disable-next-line sonarjs/cognitive-complexity
+function getSpeciesByIds(...ids) {
+  for (let index = 0; index < ids.length; index += 1) {
+    if (ids[index] === '') {
+      return emptyArray;
+    }
+  }
+  // eslint-disable-next-line array-callback-return
+  return data.species.filter(({ id }) => {
+    for (let i = 0; i < ids.length; i += 1) {
+      if (id === ids[i]) {
+        return id === ids[i];
+      }
+    }
+  });
 }
 
 module.exports = getSpeciesByIds;
