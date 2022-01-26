0b65e8622e18382cbe26bcf53740f8f435a306d3
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-01-25 23:02:58 -0300

    requisito 2 => insere funçao com HoF filter e every

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -7,6 +7,4 @@ function getAnimalsOlderThan(animal, age) {
   return specie[0].residents.every((element) => element.age >= age);
 }
 
-console.log(getAnimalsOlderThan('otters', 7));
-
 module.exports = getAnimalsOlderThan;
