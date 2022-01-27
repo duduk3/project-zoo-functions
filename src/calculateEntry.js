ed86d40ceb8d05a0149c53363a2d1de0f38a0ca2
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-01-27 08:26:34 -0300

    reuisito 6 => insere funçao que verifica qtd e idade

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,8 +1,29 @@
 const data = require('../data/zoo_data');
 
+const entrantsDudu = [
+  { name: 'edu', age: 5 },
+  { name: 'dudu', age: 5 },
+  { name: 'du', age: 5 },
+  { name: 'eduardo', age: 18 },
+  { name: 'duda', age: 18 },
+  { name: 'duduka', age: 50 },
+];
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const isChild = entrants.filter(({ age }) => age < 18);
+  const isAdult = entrants.filter(({ age }) => age >= 18 && age < 50);
+  const isSenior = entrants.filter(({ age }) => age >= 50);
+  // if (isChild.length > 0) {
+  //   console.log(isChild.length);
+  // }
+  return {
+    child: isChild.length,
+    adult: isAdult.length,
+    senior: isSenior.length,
+  };
 }
+
+console.log(countEntrants(entrantsDudu));
 
 function calculateEntry(entrants) {
   // seu código aqui
