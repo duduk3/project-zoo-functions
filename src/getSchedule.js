f2d1188ab51623992e52641813bd9d1c43f882a2
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-01-29 23:54:32 -0300

    limpa o arquivo teste

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,30 +1,56 @@
+/* eslint-disable max-len */
+/* eslint-disable arrow-body-style */
 const data = require('../data/zoo_data');
 
 const { hours } = data;
 
-const obj = {};
+const ob = {};
 
 const allDates = () => {
   Object.keys(hours).forEach((day) => {
     if (day !== 'Monday') {
       const { open } = hours[day];
       const { close } = hours[day];
-      Object.assign(obj, { [day]: { officeHour: `Open from ${open}am until ${close}pm`,
-        exhibition: data.species.filter((specie) => specie.availability.includes(day)).map((e) => (e.name)),
+      const show = data.species.filter((sp) => sp.availability.includes(day)).map((e) => (e.name));
+      Object.assign(ob, { [day]: { officeHour: `Open from ${open}am until ${close}pm`,
+        exhibition: show,
       } });
     } else {
-      Object.assign(obj, { [day]: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } });
+      Object.assign(ob, { [day]: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } });
     }
   });
-  return obj;
+  return ob;
 };
 
-console.log(allDates());
+const schedules = allDates();
+const days = Object.keys(hours);
+const animals = data.species.map((esp) => esp.name);
+let animalForDay = [];
+
+const scheduleForSpecie = (animal) => {
+  data.species.filter((specie) => {
+    if (animal === specie.name) {
+      animalForDay = specie.availability;
+      return animalForDay;
+    }
+    return animalForDay;
+  });
+};
 
 function getSchedule(scheduleTarget) {
-  if (scheduleTarget || scheduleTarget === undefined) {
-    allDates();
+  if (animals.includes(scheduleTarget)) {
+    scheduleForSpecie(scheduleTarget);
+    return animalForDay;
   }
+  if (days.includes(scheduleTarget)) {
+    const day = days.filter((d) => scheduleTarget.includes(d));
+    return {
+      [day[0]]: schedules[day[0]],
+    };
+  }
+  return schedules;
 }
+
+console.log(getSchedule('Monday'));
 
 module.exports = getSchedule;
