cc1d9bf85aa08c170509466ee6367db95c44e14d
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-01-30 19:50:15 -0300

    req10 => adiciona funçoes

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,40 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees } = data;
+
+let newEmployee = [];
+
+const filterEmployees = (arg) => {
+  const { name, id } = arg;
+  employees.forEach((employee) => {
+    if (name === employee.firstName || name === employee.lastName || id === employee.id) {
+      newEmployee = [...newEmployee, employee];
+    }
+  });
+  return newEmployee;
+};
+console.log(filterEmployees({ name: 'Spry' }));
+
+function getEmployeesCoverage(arg) {
+  const getEmployeeData = filterEmployees(arg);
+  return getEmployeeData.reduce((acc, el) => {
+    const specieName = [];
+    data.species.forEach((spec) => {
+      if (el.responsibleFor === spec.id) {
+        specieName.push(spec.name);
+      }
+    });
+    const fullName = `${el.firstName} ${el.lastName}`;
+    return {
+      id: el.id,
+      fullName,
+      species: [...specieName],
+
+    };
+  }, {});
 }
+
+console.log(getEmployeesCoverage({ name: 'Spry' }));
 
 module.exports = getEmployeesCoverage;
