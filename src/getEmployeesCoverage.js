9824c1f9744148a31cb85a1b30a834dac9173a23
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-02-10 09:36:50 -0300

    req10 => reatoracao

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
@@ -12,26 +13,38 @@ const getLocal = (id) => species
   .reduce((a, el) => el.location, []);
 
 function getEmployeesCoverage(arg) {
-  const { name: fullName, id } = arg;
   let cutName = '';
-  if (fullName) {
-    cutName = fullName.split(' ');
+  if (arg) {
+    const { name: full } = arg;
+    if (full) {
+      cutName = full.split(' ');
+    }
   }
   let obj = {};
   employees.reduce((acc, emp) => {
-    if (emp.id === id || emp.firstName === cutName[0] || emp.lastName === cutName[0]) {
+    if (!arg) {
+      obj = [...acc, ...[{
+        id: emp.id,
+        fullName: `${emp.firstName} ${emp.lastName}`,
+        species: emp.responsibleFor.map((name) => getNameById(name)),
+        location: emp.responsibleFor.map((name) => getLocal(name)),
+      }]];
+      return obj;
+    }
+    if (emp.id === arg.id || emp.firstName === cutName[0] || emp.lastName === cutName[0]) {
       obj = {
         id: emp.id,
         fullName: `${emp.firstName} ${emp.lastName}`,
         species: emp.responsibleFor.map((name) => getNameById(name)),
         location: emp.responsibleFor.map((name) => getLocal(name)),
       };
+      return obj;
     }
     return obj;
-  }, {});
+  }, []);
   return obj;
 }
 
-console.log(getEmployeesCoverage({ id: 'c1f50212-35a6-4ecd-8223-f835538526c2' }));
+console.log(getEmployeesCoverage());
 
 module.exports = getEmployeesCoverage;
